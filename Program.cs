8d6f2b38fb425f82474a15dfa26f768ff0c75831
Gabriel Jimenez <gabrielqueirozjimenez@gmail.com>
2024-01-10 00:25:15 -0300

    Desafio heranças de classes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("TESTE NOKIA:");
+Smartphone NOKIA = new Nokia(numero:"012345", modelo:"Modelo 1", imei: "111111111", memoria: 64);
+NOKIA.Ligar();
+NOKIA.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("-----------------------------------------------------------------");
+
+Console.WriteLine("TESTE IPHONE:");
+Smartphone IPHONE = new Iphone(numero:"678901", modelo:"Modelo 2", imei: "22222222", memoria: 128);
+IPHONE.Ligar();
+IPHONE.InstalarAplicativo("Telegram");
